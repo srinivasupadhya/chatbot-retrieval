96886321f91fdcc32aac0637d54dbd0d2ab57f3d
Srinivas <srinivas.upadhya@gmail.com>
2020-05-24 18:47:15 +0530

    improve predict.py

--- predict.py
+++ predict.py
@@ -1,11 +1,20 @@
+import sys
+import time
 import numpy as np
 import tensorflow as tf
 from tensorflow.contrib import predictor
 
+tf.flags.DEFINE_string("saved_model_dir", None, "Directory to load saved model from")
 tf.flags.DEFINE_string("vocab_processor_file", "./data/vocab_processor.bin", "Saved vocabulary processor file")
 FLAGS = tf.flags.FLAGS
 
+if not FLAGS.saved_model_dir:
+  print("You must specify a model directory")
+  sys.exit(1)
+
 INPUT_FEATURE_SIZE = 160
+
+start_time = time.time()
 
 def tokenizer_fn(iterator):
   return (x.split(" ") for x in iterator)
@@ -30,8 +39,11 @@ POTENTIAL_RESPONSES = ["Welcome Is there anything else I could help you with", "
                        "Good Morning What is the problem you are facing", "Looking into the issue",
                        "Hi How can I help you today", "what is the model number", "have you tried upgrading the driver"]
 
-predict_fn = predictor.from_saved_model("./saved_model/1590301632/1590301684")
+predict_fn = predictor.from_saved_model(FLAGS.saved_model_dir)
 result = []
 for r in POTENTIAL_RESPONSES:
     prediction = predict_fn(get_features(INPUT_CONTEXT, r.lower()))['output']
-    print(r.lower(), prediction)
+    result.append((r.lower(), prediction))
+
+print(sorted(result, key=lambda x: x[1], reverse=True), sep='\n')
+print("--- %s seconds ---" % (time.time() - start_time))
